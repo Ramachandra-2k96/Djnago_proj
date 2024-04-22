7c017aed8f450a4d1078f04a7786089da67d8f81
Ramachandra <ramachandraudupa.sangamone@gmail.com>
2024-04-23 04:34:24 +0530

    updated settings.py

--- djnago1/djnago1/settings.py
+++ djnago1/djnago1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^yac6bezg7$nd+yn#7#bff%y!$ikje^hpc7z6&p=*05dyen-eg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
